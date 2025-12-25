05a59a6ae0a4c3224183aa21698b6aba96a0500e
Aikakakah <145503852+Aikakakah@users.noreply.github.com>
2025-12-25 12:20:57 -0500

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -14,7 +14,13 @@ export const CHANGELOG_CONTENT = [
             "mobile support needs scaling",
             "add textures to pda",
             "ID card flickers out, fix this glitch",
-            "fix ringtone modal"
+            "fix ringtone modal",
+            "Fix PDA power off screen, glint should still show",
+            "screwdriver needs to be horizontal in drawer",
+            "double clicking notes to make big",
+            "screwdriver tip to unscrew only",
+            "removing power resistor doesnt turn off pda, issue with savegame, make sure resistors are saved",
+            "Glint shows on power off screen",
         ]
     },
     {
@@ -24,6 +30,8 @@ export const CHANGELOG_CONTENT = [
             "Added resistor color code note",
             "Fixed note pinning and drag logic",
             "Mobile support scaling (basic, fix later)",
+            "Resistors save with game, power resistor fixed."
+            
         ]
     },
     {
